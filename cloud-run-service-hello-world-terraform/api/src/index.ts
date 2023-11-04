53a9358df4862224d0393689b28809cd2b29eba9
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2023-11-04 01:20:07 -0400

    Rename recipes to demos

--- cloud-run-service-hello-world-terraform/api/src/index.ts
+++ cloud-run-service-hello-world-terraform/api/src/index.ts
@@ -4,6 +4,6 @@ import {config} from './config';
 
 app.listen(config.port, () => {
   logger.info(
-    `Google Cloud Recipes - Cloud Run Service - Hello World! server listening on port ${config.port}...`
+    `Google Cloud Demos - Cloud Run Service - Hello World! server listening on port ${config.port}...`
   );
 });
